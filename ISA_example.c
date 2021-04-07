efc5a04d219a361c23f95bcb67f6f193ff675442
MHS <gek@katherine>
2021-04-07 16:41:32 -0400

    Automatic commit.

--- ISA_example.c
+++ ISA_example.c
@@ -5,13 +5,12 @@
 #define o goto
 #define G r(S.p++)
 #define Z (S.p+=2,(((u6)r(S.p-2))<<8)+r(S.p-1))
-#define D ;switch(G&31){k 0:o h;k 1:o A;k 2:o v;k 3:o B;k 4:o V;k 5:o X;k 6: o t;k 7: o T;k 8: o oa;\
-						k 9:o os;k 10: o om;k 11:o od;k 12:o oi;k 13: o C;k 14:o j;k 15: o J;\
+#define D ;switch(G&31){k 0:o h;k 1:o A;k 2:o v;k 3:o B;k 4:o V;k 5:o X;k 6: o t;k 7: o T;\
+						k 8:o oa;k 9:o os;k 10:o om;k 11:o od;k 12:o oi;k 13: o C;k 14:o j;k 15:o J;\
 						k 16:o Q0;k 17:o Q1;k 18:o Q2;k 19:o Q3;k 20:o Q4;k 21:o Q5;k 22:o Q6;k 23:o Q7;\
 						k 24:o Q8;k 25:o Q9;k 26:o QA;k 27:o QB;k 28:o QC;k 29:o QD;k 30:o QE;k 31:o QF;}
 typedef uint8_t u8;typedef uint16_t u6;struct{u8 a;u8 b;u6 c;u6 p;u8 R;u8*y;u8 M[(1<<16)];}S={0};
 u8 r(u6 d){z S.M[d];}u8 w(u8 v, u6 d){S.M[d] = v;}
-
 u6 e(){
 S.R=0;D
 Q2:S.a&=S.b;D Q3:S.a|=S.b;D Q4:S.a^=S.b;D
