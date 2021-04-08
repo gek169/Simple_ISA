dd3d90de2016945e5b278dea9a8a9037fa4f64b7
MHS <gek@katherine>
2021-04-08 14:05:32 -0400

    Automatic commit.

--- textmode_driver.c
+++ textmode_driver.c
@@ -1,5 +1,2 @@
 #include <stdio.h>
-void di(){}
-void dcl(){}
-unsigned short gch(){return getchar();}
-void pch(unsigned short a){putchar(a);}
+void di(){}void dcl(){}unsigned short gch(){return getchar();}void pch(unsigned short a){putchar(a);}
