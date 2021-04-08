6583ab36db311f0d1b22ef68800b1b2124abe75b
MHS <gek@katherine>
2021-04-08 15:26:48 -0400

    Automatic commit.

--- d.h
+++ d.h
@@ -1,6 +1,8 @@
 #include <stdio.h>
-/*Implement your own driver here.*/
-inline void di(){}
-inline void dcl(){}
-inline unsigned short gch(){return getchar();}
-inline void pch(unsigned short a){putchar(a);}
+/*Implement your own driver here.
+use inline to get the best speed.
+*/
+void di(){}
+void dcl(){}
+unsigned short gch(){return getchar();}
+void pch(unsigned short a){putchar(a);}
