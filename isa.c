59f8e3557316cc1a51a2e0f3a671aae4e0b5b18d
MHS <gek@katherine>
2021-04-10 14:38:33 -0400

    Automatic commit.

--- isa.c
+++ isa.c
@@ -16,7 +16,7 @@
 #define Z3F ((((U)M[(((UU)c)<<8)+((UU)a&255)])<<8)+(U)M[(((UU)c)<<8)+((UU)a&255)+1])
 #define w(v,d)M[d]=v;
 #define W(v,d)M[d]=v>>8;M[d+1]=v&255;
-#define D ;switch(G&127){k 0:o h;k 1:o A;k 2:o v;k 3:o B;k 4:o V;k 5:o X;k 6:o t;k 7:o T;\
+#define D ;switch(G){k 0:o h;k 1:o A;k 2:o v;k 3:o B;k 4:o V;k 5:o X;k 6:o t;k 7:o T;\
 k 8:o oa;k 9:o os;k 10:o om;k 11:o od;k 12:o oi;k 13: o C;k 14:o j;k 15:o J;\
 k 16:o Q0;k 17:o Q1;k 18:o Q2;k 19:o Q3;k 20:o Q4;k 21:o Q5;k 22:o Q6;k 23:o Q7;\
 k 24:o Q8;k 25:o Q9;k 26:o QA;k 27:o QB;k 28:o QC;k 29:o QD;k 30:o QE;k 31:o QF;\
@@ -32,9 +32,23 @@ k 96:o V0;k 97:o V1;k 98:o V2;k 99:o V3;k 100:o V4;k 101:o V5;k 102:o V6;k 103:o
 k 104:o V8;k 105:o V9;k 106:o VA;k 107:o VB;k 108:o VC;k 109:o VD;k 110:o VE;k 111:o VF;\
 k 112:o W0;k 113:o W1;k 114:o W2;k 115:o W3;k 116:o W4;k 117:o W5;k 118:o W6;k 119:o W7;\
 k 120:o W8;k 121:o W9;k 122:o WA;k 123:o WB;k 124:o WC;k 125:o WD;k 126:o WE;k 127:o WF;\
-}
+k 128:k 129:k 130:k 131:k 132:k 133:k 134:k 135:k 136:k 137:\
+k 138:k 139:k 140:k 141:k 142:k 143:k 144:k 145:k 146:k 147:\
+k 148:k 149:k 150:k 151:k 152:k 153:k 154:k 155:k 156:k 157:\
+k 158:k 159:k 160:k 161:k 162:k 163:k 164:k 165:k 166:k 167:\
+k 168:k 169:k 170:k 171:k 172:k 173:k 174:k 175:k 176:k 177:\
+k 178:k 179:k 180:k 181:k 182:k 183:k 184:k 185:k 186:k 187:\
+k 188:k 189:k 190:k 191:k 192:k 193:k 194:k 195:k 196:k 197:\
+k 198:k 199:k 200:k 201:k 202:k 203:k 204:k 205:k 206:k 207:\
+k 208:k 209:k 210:k 211:k 212:k 213:k 214:k 215:k 216:k 217:\
+k 218:k 219:k 220:k 221:k 222:k 223:k 224:k 225:k 226:k 227:\
+k 228:k 229:k 230:k 231:k 232:k 233:k 234:k 235:k 236:k 237:\
+k 238:k 239:k 240:k 241:k 242:k 243:k 244:k 245:k 246:k 247:\
+k 248:k 249:k 250:k 251:k 252:k 253:k 254:k 255:o h;}
 typedef unsigned long UU;typedef unsigned char u;typedef unsigned short U;U a,b,c,p,P;u R,pp,M[(1<<24)];FILE*F;
 e(){R=0;p=0;pp=0;a=0;b=0;P=0;di();QE:D
+V5:V6:V7:V8:V9:VA:VB:VC:VD:VE:VF:W0:W1:W2:W3:W4:W5:W6:W7:W8:W9:WA:WB:WC:WD:WE:WF:
+h:dcl();z 0;
 Q2:a&=b;D
 Q3:a|=b;D
 Q4:a^=b;D
@@ -134,8 +148,7 @@ V1:b=ZR;D
 V2:c=ZR;D
 V3:P-=1;a=r(P)D
 V4:P-=1;b=r(P)D
-V5:V6:V7:V8:V9:VA:VB:VC:VD:VE:VF:W0:W1:W2:W3:W4:W5:W6:W7:W8:W9:WA:WB:WC:WD:WE:WF:
-h:dcl();z 0;}
+}
 main(int rc,char**rv){UU i=0,j;if(rc<2)z 1;
 for(F=fopen(rv[1],"rb");!feof(F);){M[i++]=fgetc(F);i&=0xffffff;if(i==0)break;}
 for(i=e();i<(1<<24)-31&&rc>2;i+=32)for(j=i,printf("%s\n%04lx|",(i&255)?"":"\n~",i);j<i+32;j++)printf("%02x%c",M[j],((j+1)%8)?' ':'|');
