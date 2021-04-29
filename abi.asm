fe90d51885945496fca7ea403684aae53be9ce92
MHS <gek@katherine>
2021-04-29 01:53:02 -0400

    Automatic commit.

--- abi.asm
+++ abi.asm
@@ -1,14 +1,15 @@
-//library functions.
-//What region should the library start on?
-//ten regions are reserved for it.
-//variables it uses are also in this range.
-VAR#CALCULATE_REGION_LOCATION#asm_muleq#\_arg1_ADDR#0#;|asm_pleq#\_arg1_ADDR#0x10000#;|asm_muleq#\_arg1_ADDR#\_arg1#;
+//define the regions to use.
+//we also need to define these ADDR variables, just set them to zero.
 VAR#DATA_REGION_1#0x2A
 VAR#DATA_REGION_1_ADDR#0
 VAR#__START__#0x1
 VAR#__START___ADDR#0
 VAR#LIBC_START#0x12
 VAR#LIBC_START_ADDR#0
+
+
+VAR#CALCULATE_REGION_LOCATION#asm_muleq#\_arg1_ADDR#0#;|asm_pleq#\_arg1_ADDR#0x10000#;|asm_muleq#\_arg1_ADDR#\_arg1#;
+
 asm_call#CALCULATE_REGION_LOCATION#DATA_REGION_1##;
 asm_call#CALCULATE_REGION_LOCATION#__START__##;
 asm_call#CALCULATE_REGION_LOCATION#LIBC_START##;
@@ -16,17 +17,25 @@ asm_vars;
 
 
 //macros
-VAR#goto#sc _arg1;jmp;
 VAR#ZERO_STACK_POINTER#astp;popa;
-
-
-
-//we assume that register a is being used.
 VAR#POP_FARPTR_VARIABLE#	blpop;apop;ca;
 VAR#PUSH_FARPTR_VARIABLE#	ac;apush;blpush;
-VAR#ISEQ_CONST#	load__arg1;llb _arg2;cmp;
+//generic loads and stores for far variables at constant addresses.
+VAR#fetch_far_short#sc _arg1;llb _arg2; farillda;
+VAR#fetch_far_byte#sc _arg1;llb _arg2; farilda;
+VAR#put_far_short#sc _arg1;llb _arg2; faristla;
+VAR#put_far_byte#sc _arg1;llb _arg2; farista;
+//Index into an array whose pointer you know. Argument 3 is the macro to load the indexing variable.
+VAR#fetch_far_short_array#load__arg3;lb2;mul;sc_arg1;llb_arg2;add;farilldb;ab;
+VAR#fetch_far_byte_array#load__arg3;sc_arg1;llb_arg2;add;ba;farilda;
+//There are now two arguments.
+//Argument 3 is the index, argument 4 is the variable name to store.
+VAR#put_far_short_array#	load__arg4;alpush;\
+							load__arg3;lb2;mul;sc_arg1;llb_arg2;add;ba;alpop;faristla;
+VAR#put_far_byte_array#	load__arg4;apush;\
+						load__arg3;sc_arg1;llb_arg2;add;ba;apop;farista;
 
-//if the variable referenced by argument 1 is 
+VAR#ISEQ_CONST#	load__arg1;llb _arg2;cmp;
 VAR#goto_ifeq_const#		sc _arg2;load__arg1;llb%_arg3%;cmp;jmpifeq;
 VAR#goto_ifneq_const#		sc _arg2;load__arg1;llb%_arg3%;cmp;jmpifneq;
 VAR#goto_ifgreater_const#	sc _arg2;load__arg1;llb%_arg3%;cmp;llb%2%;cmp;jmpifeq;
@@ -80,25 +89,25 @@ section __START___ADDR; asm_begin_region_restriction;
 VAR#proc_main#sc%__START___ADDR%;lla %__START__%;farcall;
 ZERO_STACK_POINTER;
 
-//we need an i for this loop
-//user variables go in 0x20000.
-//the user can also use the entire 0xFxXXxx super-region.
-//
+
 VAR#load_main_i#sc%0x2%;llb%0x0000%;farillda;
 VAR#store_main_i#sc%0x2%;llb%0x0000%;faristla;
+
 lla %0%;store_main_i;
 laDATA_REGION_1;apush;lla%msg_req_string%;alpush;
 proc_puts;
-//end 
+
 halt;
 asm_end_region_restriction;
 
 
+
+
+
+
+
+
+
+
 //BOOTLOADER~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-section 0
-proc_main;
-
-
-
-
-
+section 0;proc_main;
