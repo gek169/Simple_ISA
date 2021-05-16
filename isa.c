d0885f94e6be96a50a5838e96a046c8a67d5c7f5
gek <6974902+gordonfreeman424@users.noreply.github.com>
2021-05-16 03:20:07 -0400

    Automatic commit.

--- isa.c
+++ isa.c
@@ -2,9 +2,7 @@
 #include <stdio.h>
 typedef unsigned long UU;typedef unsigned char u;typedef unsigned short U;u R,M[(1<<24)];FILE*F;
 #include "d.h"
-#define z return
 #define k case
-#define o goto
 #define PP ((UU)(pp<<16))
 #define G M[PP+p++]
 #define r(d)M[d]
@@ -17,22 +15,22 @@ typedef unsigned long UU;typedef unsigned char u;typedef unsigned short U;u R,M[
 #define Z3F ((((U)M[(((UU)c&255)<<16)+((UU)a)])<<8)+(U)M[(((UU)c&255)<<16)+(UU)(a+1)])
 #define w(v,d)M[d]=v;
 #define W(v,d)M[d]=v>>8;M[(d+1)&0xFFffFF]=v&255;
-#define D ;switch(G){k 0:o h;k 1:o A;k 2:o v;k 3:o B;k 4:o V;k 5:o X;k 6:o t;k 7:o T;\
-k 8:o oa;k 9:o os;k 10:o om;k 11:o od;k 12:o oi;k 13: o C;k 14:o j;k 15:o J;\
-k 16:o Q0;k 17:o Q1;k 18:o Q2;k 19:o Q3;k 20:o Q4;k 21:o Q5;k 22:o Q6;k 23:o Q7;\
-k 24:o Q8;k 25:o Q9;k 26:o QA;k 27:o QB;k 28:o QC;k 29:o QD;k 30:o QE;k 31:o QF;\
-k 32:o R0;k 33:o R1;k 34:o R2;k 35:o R3;k 36:o R4;k 37:o R5;k 38:o R6;k 39:o R7;\
-k 40:o R8;k 41:o R9;k 42:o RA;k 43:o RB;k 44:o RC;k 45:o RD;k 46:o RE;k 47:o RF;\
-k 48:o S0;k 49:o S1;k 50:o S2;k 51:o S3;k 52:o S4;k 53:o S5;k 54:o S6;k 55:o S7;\
-k 56:o S8;k 57:o S9;k 58:o SA;k 59:o SB;k 60:o SC;k 61:o SD;k 62:o SE;k 63:o SF;\
-k 64:o T0;k 65:o T1;k 66:o T2;k 67:o T3;k 68:o T4;k 69:o T5;k 70:o T6;k 71:o T7;\
-k 72:o T8;k 73:o T9;k 74:o TA;k 75:o TB;k 76:o TC;k 77:o TD;k 78:o TE;k 79:o TF;\
-k 80:o U0;k 81:o U1;k 82:o U2;k 83:o U3;k 84:o U4;k 85:o U5;k 86:o U6;k 87:o U7;\
-k 88:o U8;k 89:o U9;k 90:o UA;k 91:o UB;k 92:o UC;k 93:o UD;k 94:o UE;k 95:o UF;\
-k 96:o V0;k 97:o V1;k 98:o V2;k 99:o V3;k 100:o V4;k 101:o V5;k 102:o V6;k 103:o V7;\
-k 104:o V8;k 105:o V9;k 106:o VA;k 107:o VB;k 108:o VC;k 109:o VD;k 110:o VE;k 111:o VF;\
-k 112:o W0;k 113:o W1;k 114:o W2;k 115:o W3;k 116:o W4;k 117:o W5;k 118:o W6;k 119:o W7;\
-k 120:o W8;k 121:o W9;k 122:o WA;k 123:o WB;k 124:o WC;k 125:o WD;k 126:o WE;k 127:o WF;\
+#define D ;switch(G){k 0:goto G_HALT;k 1:goto G_LDA;k 2:goto G_LA;k 3:goto G_LDB;k 4:goto G_LB;k 5:goto G_SC;k 6:goto G_STA;k 7:goto G_STB;\
+k 8:goto oa;k 9:goto os;k 10:goto om;k 11:goto od;k 12:goto oi;k 13: goto C;k 14:goto j;k 15:goto J;\
+k 16:goto G_GETCHAR;k 17:goto G_PUTCHAR;k 18:goto G_AND;k 19:goto G_OR;k 20:goto G_XOR;k 21:goto G_LSHIFT;k 22:goto G_RSHIFT;k 23:goto G_ILDA;\
+k 24:goto G_ILDB;k 25:goto G_CAB;k 26:goto G_AB;k 27:goto G_BA;k 28:goto G_ALC;k 29:goto G_AHC;k 30:goto G_NOP;k 31:goto G_CBA;\
+k 32:goto G_LLA;k 33:goto G_ILLDA;k 34:goto G_LLB;k 35:goto G_ILLDB;k 36:goto G_ILLDAA;k 37:goto G_ILLDBB;k 38:goto G_ILLDAB;k 39:goto G_ILLDBA;\
+k 40:goto G_CA;k 41:goto G_CB;k 42:goto G_AC;k 43:goto G_BC;k 44:goto G_ISTA;k 45:goto G_ISTB;k 46:goto G_ISTLA;k 47:goto G_ISTLB;\
+k 48:goto G_JMP;k 49:goto G_STLA;k 50:goto G_STLB;k 51:goto G_STC;k 52:goto G_PUSH;k 53:goto G_POP;k 54:goto G_PUSHA;k 55:goto G_POPA;\
+k 56:goto G_ASTP;k 57:goto G_BSTP;k 58:goto G_COMPL;k 59:goto G_CPC;k 60:goto SC;k 61:goto SD;k 62:goto SE;k 63:goto SF;\
+k 64:goto T0;k 65:goto T1;k 66:goto T2;k 67:goto T3;k 68:goto T4;k 69:goto T5;k 70:goto T6;k 71:goto T7;\
+k 72:goto T8;k 73:goto T9;k 74:goto TA;k 75:goto TB;k 76:goto TC;k 77:goto TD;k 78:goto TE;k 79:goto TF;\
+k 80:goto U0;k 81:goto U1;k 82:goto U2;k 83:goto U3;k 84:goto U4;k 85:goto U5;k 86:goto U6;k 87:goto U7;\
+k 88:goto U8;k 89:goto U9;k 90:goto UA;k 91:goto UB;k 92:goto UC;k 93:goto UD;k 94:goto UE;k 95:goto UF;\
+k 96:goto V0;k 97:goto V1;k 98:goto V2;k 99:goto V3;k 100:goto V4;k 101:goto V5;k 102:goto V6;k 103:goto V7;\
+k 104:goto V8;k 105:goto V9;k 106:goto VA;k 107:goto VB;k 108:goto VC;k 109:goto VD;k 110:goto VE;k 111:goto VF;\
+k 112:goto W0;k 113:goto W1;k 114:goto W2;k 115:goto W3;k 116:goto W4;k 117:goto W5;k 118:goto W6;k 119:goto W7;\
+k 120:goto W8;k 121:goto W9;k 122:goto WA;k 123:goto WB;k 124:goto WC;k 125:goto WD;k 126:goto WE;k 127:goto WF;\
 k 128:k 129:k 130:k 131:k 132:k 133:k 134:k 135:k 136:k 137:\
 k 138:k 139:k 140:k 141:k 142:k 143:k 144:k 145:k 146:k 147:\
 k 148:k 149:k 150:k 151:k 152:k 153:k 154:k 155:k 156:k 157:\
@@ -45,67 +43,71 @@ k 208:k 209:k 210:k 211:k 212:k 213:k 214:k 215:k 216:k 217:\
 k 218:k 219:k 220:k 221:k 222:k 223:k 224:k 225:k 226:k 227:\
 k 228:k 229:k 230:k 231:k 232:k 233:k 234:k 235:k 236:k 237:\
 k 238:k 239:k 240:k 241:k 242:k 243:k 244:k 245:k 246:k 247:\
-k 248:k 249:k 250:k 251:k 252:k 253:k 254:k 255:o h;}
-e(){register u pp=0;register U a=0,b=0,c=0,p=0,P=0;R=0;di();QE:D
+k 248:k 249:k 250:k 251:k 252:k 253:k 254:k 255:goto G_HALT;}
+int e(){
+	register u pp=0;register U a=0,b=0,c=0,p=0,P=0;R=0;
+	di();
+
+	G_NOP:D
 V5:V6:V7:V8:V9:VA:VB:VC:VD:VE:VF:W0:W1:W2:W3:W4:W5:W6:W7:W8:W9:WA:WB:WC:WD:WE:WF:
-h:dcl();z 0;
-Q2:a&=b;D
-Q3:a|=b;D
-Q4:a^=b;D
-Q0:a=gch()D
-Q1:pch(a)D
-Q5:a<<=b&15;D
-Q6:a>>=b&15;D
-Q7:a=r(c)D
-Q8:b=r(c)D
-Q9:c=((a&255)<<8)+(b&255)D
-QA:a=b;D
-QB:b=a;D
-QC:a=c&0xff;D
-QD:a=(c>>8)&255;D
-QF:c=((b&255)<<8)+(a&255)D
-R0:a=Z;D
-R1:a=Z2;D
-R2:b=Z;D
-R3:b=Z2;D
-R4:a=Z3;D
-R5:b=Z4;D
-R6:a=Z4;D
-R7:b=Z3;D
-R8:c=a;D
-R9:c=b;D
-RA:a=c;D
-RB:b=c;D
-RC:w(a,c)D
-RD:w(b,c)D
-RE:W(a,c)D
-RF:W(b,c)D
-S0:p=c;D
-S1:W(a,Z)D
-S2:W(b,Z)D
-S3:W(c,Z)D
-S4:P+=Z;D
-S5:P-=Z;D
-S6:P+=a;D
-S7:P-=a;D
-S8:a=P;D
-S9:b=P;D
-SA:a=~a;D
-SB:c=p;D
-A:a=r(Z)D
-v:a=G;D
-B:b=r(Z)D
-V:b=G;D
-X:c=Z;D
-t:w(a,Z)D
-T:w(b,Z)D
+G_HALT:dcl();return 0;
+G_AND:a&=b;D
+G_OR:a|=b;D
+G_XOR:a^=b;D
+G_GETCHAR:a=gch()D
+G_PUTCHAR:pch(a)D
+G_LSHIFT:a<<=b&15;D
+G_RSHIFT:a>>=b&15;D
+G_ILDA:a=r(c)D
+G_ILDB:b=r(c)D
+G_CAB:c=((a&255)<<8)+(b&255)D
+G_AB:a=b;D
+G_BA:b=a;D
+G_ALC:a=c&0xff;D
+G_AHC:a=(c>>8)&255;D
+G_CBA:c=((b&255)<<8)+(a&255)D
+G_LLA:a=Z;D
+G_ILLDA:a=Z2;D
+G_LLB:b=Z;D
+G_ILLDB:b=Z2;D
+G_ILLDAA:a=Z3;D
+G_ILLDBB:b=Z4;D
+G_ILLDAB:a=Z4;D
+G_ILLDBA:b=Z3;D
+G_CA:c=a;D
+G_CB:c=b;D
+G_AC:a=c;D
+G_BC:b=c;D
+G_ISTA:w(a,c)D
+G_ISTB:w(b,c)D
+G_ISTLA:W(a,c)D
+G_ISTLB:W(b,c)D
+G_JMP:p=c;D
+G_STLA:W(a,Z)D
+G_STLB:W(b,Z)D
+G_STC:W(c,Z)D
+G_PUSH:P+=Z;D
+G_POP:P-=Z;D
+G_PUSHA:P+=a;D
+G_POPA:P-=a;D
+G_ASTP:a=P;D
+G_BSTP:b=P;D
+G_COMPL:a=~a;D
+G_CPC:c=p;D
+G_LDA:a=r(Z)D
+G_LA:a=G;D
+G_LDB:b=r(Z)D
+G_LB:b=G;D
+G_SC:c=Z;D
+G_STA:w(a,Z)D
+G_STB:w(b,Z)D
 j:if(a==1)p=c;D
 J:if(a!=1)p=c;D
 oa:a+=b;D
 os:a-=b;D
 om:a*=b;D
-od:if(b!=0)a/=b;else{R=1;o h;}D
-oi:if(b!=0)a%=b;else{R=1;o h;}D
+od:if(b!=0)a/=b;else{R=1;goto G_HALT;}D
+oi:if(b!=0)a%=b;else{R=1;goto G_HALT;}D
 C:if(a<b)a=0;else if(a>b)a=2;else a=1;D
 SE:a=Z2F;D
 SF:W(a,((((UU)c&255)<<16)+((UU)b)))D
@@ -149,7 +151,15 @@ V2:c=ZR;D
 V3:P-=1;a=r(P)D
 V4:P-=1;b=r(P)D
 }
-main(int rc,char**rv){UU i=0,j;if(rc<2)z 1;
-for(F=fopen(rv[1],"rb");!feof(F);){M[i++]=fgetc(F);i&=0xffffff;if(i==0)break;}
-for(i=e();i<(1<<24)-31&&rc>2;i+=32)for(j=i,printf("%s\n%04lx|",(i&255)?"":"\n~",i);j<i+32;j++)printf("%02x%c",M[j],((j+1)%8)?' ':'|');
-if(R)puts("\n<Errfl>\n");}
+int main(int rc,char**rv){
+	UU i=0,j;
+	if(rc<2)return 1;
+	for(F=fopen(rv[1],"rb");!feof(F);){
+		M[i++]=fgetc(F);i&=0xffffff;if(i==0)break;
+	}
+	fclose(F);
+	for(i=e();i<(1<<24)-31&&rc>2;i+=32)	
+		for(j=i,printf("%s\n%04lx|",(i&255)?"":"\n~",i);j<i+32;j++)
+			printf("%02x%c",M[j],((j+1)%8)?' ':'|');
+	if(R)puts("\n<Errfl>\n");
+}
