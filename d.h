d505209caec235a1edccf99edbe34aa5e088aaaf
gek169 <gek169>
2021-05-29 08:36:14 -0400

    Automatic commit.

--- d.h
+++ d.h
@@ -36,5 +36,53 @@ static unsigned short interrupt(unsigned short a,
 			for(j=i,printf("%s\r\n%04lx|",(i&255)?"":"\r\n~",i);j<i+32;j++)
 					printf("%02x%c",M[j],((j+1)%8)?' ':'|');
 	}
+	if(a == 0xffFE){ /*Disk Read.*/
+		UU i; char buf[0x10000];
+		for(i = 0; i<0x10000; i++){
+			UU ind = ( (((UU)c&255)<<16)+((UU)b) + i) & 0xffFFff;
+			buf[i] = M[ind];
+			if(M[ind] == '\0')break;
+		}
+		buf[0xFFff] = '\0';
+		/*
+			Attempt to load it into the segment.
+		*/
+		{FILE* ff;size_t len;size_t n_pages;
+			ff = fopen(buf, "rb");
+			if(!ff) return 0;
+			fseek(ff, 0, SEEK_END);
+			len = ftell(ff);
+			fseek(ff, 0, SEEK_SET);
+			n_pages = (len + 255)/256;
+			if(n_pages == 0) return 0;
+			if(SEGMENT_PAGES < n_pages){
+				SEGMENT = realloc(SEGMENT, 0x100 * n_pages);
+				SEGMENT_PAGES = n_pages;
+				if(!SEGMENT){puts("Failed Malloc");exit(1);}
+			}
+			fread(SEGMENT, 1, len, ff);
+			fclose(ff);
+		}
+		return 1;
+	}
+	if(a == 0xffFD){ /*Disk Write*/
+		UU i; char buf[0x10000];
+		for(i = 0; i<0x10000; i++){
+			UU ind = ( (((UU)c&255)<<16)+((UU)b) + i) & 0xffFFff;
+			buf[i] = M[ind];
+			if(M[ind] == '\0')break;
+		}
+		buf[0xFFff] = '\0';
+		/*
+			Attempt to store the segment.
+		*/
+		{FILE* ff;
+			ff = fopen(buf, "wb");
+			if(!ff) return 0;
+			fwrite(SEGMENT, 1, ((size_t)SEGMENT_PAGES) * 256, ff);
+			fclose(ff);
+		}
+		return 1;
+	}
 	return a;
 }
