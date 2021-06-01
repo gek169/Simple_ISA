a9f3319ded1747e8d72a39ea6c3ddb4783f3ad20
gek169 <gek169>
2021-06-01 14:10:52 -0400

    Automatic commit.

--- clock.asm
+++ clock.asm
@@ -15,49 +15,50 @@ VAR#st_iteration_count#	farstla %&0x440000%;
 section 0x20000;asm_begin_region_restriction;
 VAR#proc_printbytehex#sc 0,0;la 2;farcall; 
 //retrieve our argument.
-astp;lb4;sub;ca;ilda;
-apush;
-lb 4;rsh;
-lb0xf;and;
-lb 7;mul;
+	astp;lb4;sub;ca;ilda;
+//push it
+	apush;
+	lb 4;rsh;
+	lb0xf;and;
+	lb 7;mul;
 llb %printbytehex_jmptable_1%; add;ca;jmp;
 VAR#printbytehex_jmptable_1#@
-la0x30;putchar;sc%printbytehex_jmptable_1_end%;jmp
-la0x31;putchar;sc%printbytehex_jmptable_1_end%;jmp
-la0x32;putchar;sc%printbytehex_jmptable_1_end%;jmp
-la0x33;putchar;sc%printbytehex_jmptable_1_end%;jmp
-la0x34;putchar;sc%printbytehex_jmptable_1_end%;jmp
-la0x35;putchar;sc%printbytehex_jmptable_1_end%;jmp
-la0x36;putchar;sc%printbytehex_jmptable_1_end%;jmp
-la0x37;putchar;sc%printbytehex_jmptable_1_end%;jmp
-la0x38;putchar;sc%printbytehex_jmptable_1_end%;jmp
-la0x39;putchar;sc%printbytehex_jmptable_1_end%;jmp
-la0x41;putchar;sc%printbytehex_jmptable_1_end%;jmp
-la0x42;putchar;sc%printbytehex_jmptable_1_end%;jmp
-la0x43;putchar;sc%printbytehex_jmptable_1_end%;jmp
-la0x44;putchar;sc%printbytehex_jmptable_1_end%;jmp
-la0x45;putchar;sc%printbytehex_jmptable_1_end%;jmp
-la0x46;putchar;sc%printbytehex_jmptable_1_end%;jmp
+	la0x30;putchar;sc%printbytehex_jmptable_1_end%;jmp
+	la0x31;putchar;sc%printbytehex_jmptable_1_end%;jmp
+	la0x32;putchar;sc%printbytehex_jmptable_1_end%;jmp
+	la0x33;putchar;sc%printbytehex_jmptable_1_end%;jmp
+	la0x34;putchar;sc%printbytehex_jmptable_1_end%;jmp
+	la0x35;putchar;sc%printbytehex_jmptable_1_end%;jmp
+	la0x36;putchar;sc%printbytehex_jmptable_1_end%;jmp
+	la0x37;putchar;sc%printbytehex_jmptable_1_end%;jmp
+	la0x38;putchar;sc%printbytehex_jmptable_1_end%;jmp
+	la0x39;putchar;sc%printbytehex_jmptable_1_end%;jmp
+	la0x41;putchar;sc%printbytehex_jmptable_1_end%;jmp
+	la0x42;putchar;sc%printbytehex_jmptable_1_end%;jmp
+	la0x43;putchar;sc%printbytehex_jmptable_1_end%;jmp
+	la0x44;putchar;sc%printbytehex_jmptable_1_end%;jmp
+	la0x45;putchar;sc%printbytehex_jmptable_1_end%;jmp
+	la0x46;putchar;sc%printbytehex_jmptable_1_end%;jmp
 VAR#printbytehex_jmptable_1_end#@
 apop;lb 15;and;lb7;mul;
 llb%printbytehex_jmptable_2%;add;ca;jmp;
 VAR#printbytehex_jmptable_2#@
-la0x30;putchar;sc%printbytehex_jmptable_2_end%;jmp
-la0x31;putchar;sc%printbytehex_jmptable_2_end%;jmp
-la0x32;putchar;sc%printbytehex_jmptable_2_end%;jmp
-la0x33;putchar;sc%printbytehex_jmptable_2_end%;jmp
-la0x34;putchar;sc%printbytehex_jmptable_2_end%;jmp
-la0x35;putchar;sc%printbytehex_jmptable_2_end%;jmp
-la0x36;putchar;sc%printbytehex_jmptable_2_end%;jmp
-la0x37;putchar;sc%printbytehex_jmptable_2_end%;jmp
-la0x38;putchar;sc%printbytehex_jmptable_2_end%;jmp
-la0x39;putchar;sc%printbytehex_jmptable_2_end%;jmp
-la0x41;putchar;sc%printbytehex_jmptable_2_end%;jmp
-la0x42;putchar;sc%printbytehex_jmptable_2_end%;jmp
-la0x43;putchar;sc%printbytehex_jmptable_2_end%;jmp
-la0x44;putchar;sc%printbytehex_jmptable_2_end%;jmp
-la0x45;putchar;sc%printbytehex_jmptable_2_end%;jmp
-la0x46;putchar;sc%printbytehex_jmptable_2_end%;jmp
+	la0x30;putchar;sc%printbytehex_jmptable_2_end%;jmp
+	la0x31;putchar;sc%printbytehex_jmptable_2_end%;jmp
+	la0x32;putchar;sc%printbytehex_jmptable_2_end%;jmp
+	la0x33;putchar;sc%printbytehex_jmptable_2_end%;jmp
+	la0x34;putchar;sc%printbytehex_jmptable_2_end%;jmp
+	la0x35;putchar;sc%printbytehex_jmptable_2_end%;jmp
+	la0x36;putchar;sc%printbytehex_jmptable_2_end%;jmp
+	la0x37;putchar;sc%printbytehex_jmptable_2_end%;jmp
+	la0x38;putchar;sc%printbytehex_jmptable_2_end%;jmp
+	la0x39;putchar;sc%printbytehex_jmptable_2_end%;jmp
+	la0x41;putchar;sc%printbytehex_jmptable_2_end%;jmp
+	la0x42;putchar;sc%printbytehex_jmptable_2_end%;jmp
+	la0x43;putchar;sc%printbytehex_jmptable_2_end%;jmp
+	la0x44;putchar;sc%printbytehex_jmptable_2_end%;jmp
+	la0x45;putchar;sc%printbytehex_jmptable_2_end%;jmp
+	la0x46;putchar;sc%printbytehex_jmptable_2_end%;jmp
 VAR#printbytehex_jmptable_2_end#@
 farret;
 
