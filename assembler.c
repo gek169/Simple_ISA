5940aff6e921c39a6cf4451bbff4e27cbe75a3ec
gek169 <gek169>
2021-05-31 23:48:54 -0400

    Automatic commit.

--- assembler.c
+++ assembler.c
@@ -957,7 +957,7 @@ int main(int argc, char** argv){
 			ASM_PUTS("Optional argument: -C: display compiletime environment information (What C compiler you used) as well as Author.");
 			ASM_PUTS("Optional argument: -run: Build and Execute assembly file, like -i. Compatible with shebangs on *nix machines.\nTry adding `#!/usr/bin/sisa16_asm -run` to the start of your programs!");
 			ASM_PUTS("Optional argument: -v, -h, --help, --version: This printout.");
-			ASM_PUTS("\n\nSISA-16 Assembler and Emulator in Pure Glorious C89, Version 1.3");
+			ASM_PUTS("\n\nSISA-16 Assembler and Emulator in Pure Glorious C89, Version 1.5");
 			ASM_PUTS("Authored by DMHSW for the Public Domain. Enjoy.\n\n");
 			return 1;
 		}
