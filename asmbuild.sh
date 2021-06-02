d67c4e07321127082571a26fabda96537f964443
gek169 <gek169>
2021-06-01 22:58:07 -0400

    Automatic commit.

---
+++ asmbuild.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+IFS=$'\n' ASMFILE=$(ls -1|grep '.\.asm')
+for filename in $ASMFILE
+do
+ echo "<SHELL> Assembling $filename"
+ name=$(echo "$filename" | cut -f 1 -d '.')
+ sisa16_asm -i $filename -o $name.bin
+done
