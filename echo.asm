5a6d0267dcc200df517c7aea31ce59584bb622ec
[MHS] <gek169@github.com>
2021-06-03 14:36:47 -0400

    Automatic commit.

--- echo.asm
+++ echo.asm
@@ -6,11 +6,11 @@
 VAR#looptop#1,0xf0
 VAR#GOTO_TOP#sc looptop;jmp;
 VAR#jumpc#jmp;
-VAR#IS_A_NEWLINE#			sta0,0;lb0xa;cmp;
-VAR#IS_A_CR#				sta0,0;lb0xd;cmp;
-VAR#IS_A_Q#					sta0,0;lb0x51;cmp;
-VAR#GET_A_BACK#				lda0,0;
-
+VAR#IS_A_NEWLINE#			sta%50%;lb0xa;cmp;
+VAR#IS_A_CR#				sta%50%;lb0xd;cmp;
+VAR#IS_A_Q#					sta%50%;lb0x51;cmp;
+VAR#IS_A_R#					sta%50%;lb0x52;cmp;
+VAR#GET_A_BACK#				lda%50%;
 
 
 asm_print %/32%;
@@ -30,55 +30,62 @@ section 0x1F0
 //Read character from stdin into A and echo it backIgnore newlines.
 	getchar;
 	IS_A_NEWLINE;	jmpifeq;GET_A_BACK;
-//	IS_A_CR;		jmpifeq;GET_A_BACK;
-	sc %Lbl_BONUSDUCKS%;IS_A_Q;jmpifeq;GET_A_BACK;
+	sc %Lbl_BONUSDUCKS%;	IS_A_Q;jmpifeq;GET_A_BACK;
+	sc %Lbl_Emulator%;		IS_A_R;jmpifeq;GET_A_BACK;
 	sc 1,0xF0;
 
 #Simple loop
 
 
 	
-//prepare the loop by preparing our looping variable at 5,
+//prepare the loop by preparing our looping variable at 59,
 //and the address register C to repeatedly jump to the top.
-	lb 0;stb 0,5;
-	sta 0,0;
+	lb 0;
+	stb %59%;
+	sta %50%;
 //small loop.
 VAR#miniLoopTop#@
 //Perform wizardry
-	lda %0%;
+	lda %50%;
 		lb 8;
 		//lb 1;
 		//add;
 		imul13.3;
-	sta %0%;
+	sta %50%;
 //if this is less than 127, then we go to dont manip.
 		lb 127;cmp;lb0;cmp;sc %dont_manip%;jmpifeq;
 VAR#still_above_127#@
 //generate negative one, twos complement, and put it in b.
 	la 1;compl;lb1;add;ba;
-	lda %0%;
+	lda %50%;
 		add;
-	sta %0%;
+	sta %50%;
 //jump to still_above if the value is >= 127.
 	lb 127;cmp;lb0;cmp;sc %dont_manip%;jmpifneq;
 VAR#dont_manip#@
-	//if a is not Q, then we dont subtract from it.
-	lda %0%;IS_A_Q;sc %A_IS_notQ%;jmpifneq;
-	lda %0%;lb5;sub;sta %0%;
+	//if a is not invalid, we dont subtract from it.
+	lda %50%;IS_A_Q;sc %A_IS_notQ%;jmpifneq;
+	lda %50%;
+	lb5;
+	sub;
+	sta %50%;
 
 VAR#A_IS_notQ#@
 	
-	lda %0%;putchar;
+	lda %50%;putchar;
 //perform our variable increment.
-	lb1;lda%5%;add;sta %5%;
+	lb1;lda%59%;add;sta %59%;
 //if A is less than 3 value, jump to the small loop
 	sc %miniLoopTop%;llb %4%;cmp;lb 0;cmp;jmpifeq;
 
 
 //signify the end of the loop by printing some newlines.
 	GOTO_TOP
-#Unreachable HALT insn
-halt
+//separating nops.
+nop;nop;nop;nop;nop;
+nop;nop;nop;nop;nop;
+nop;nop;nop;nop;nop;
+nop;nop;nop;nop;nop;
 
 //if you type in a q, you get ten Qs.
 VAR#Lbl_BONUSDUCKS#@
@@ -95,3 +102,15 @@ VAR#numQs#10
 lrx0 %/12%;
 lrx1 %/0%;
 rxidiv;
+
+VAR#Lbl_Emulator#@
+//print an R.
+la0x52;
+putchar;
+lla %0xffFF%;
+//execute the emulator!
+emulate;
+sc %0x1f0%;jmp;
+//So that a disassembly will not show beyond this point.
+halt;halt;
+halt;halt;
