aeeaefd41f5119d1aa0e2af7ad8e2aebdb6a3775
[MHS] <gek169@github.com>
2021-06-02 23:03:17 -0400

    Automatic commit.

--- rbytes.c
+++ rbytes.c
@@ -3,6 +3,6 @@
 #include <stdlib.h>
 #include <time.h>
 int main(){
-	long i = 0;srand(time(NULL));for(;i<0xffff;i++){putchar(rand()&255);}
+	long i = 0;srand(time(NULL));for(;i<0x1000000;i++){putchar(rand()&255);}
 	return 0;
 }
