09f07c7a3dcdd7853292f6ac8875c2af18a92f92
gek <6974902+gordonfreeman424@users.noreply.github.com>
2021-06-05 22:06:01 -0400

    Automatic commit.

--- a.c
+++ a.c
@@ -1,6 +1,6 @@
 #!/usr/local/bin/tcc -run -lncurses -DUSE_UNSIGNED_INT -DUSE_NCURSES
 /*
-	A very quick, convenient way to run the assembler.
+	A very quick, convenient way to run the assembler with built-in emulator.
 	Truly we are blessed 
 */
 #include "assembler.c"
---
+++ atcc.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#this version of the script is used if you have sisa16_asm installed.
+IFS=$'\n' ASMFILE=$(ls -1|grep '.\.asm')
+for filename in $ASMFILE
+do
+ echo "<SHELL> Assembling $filename"
+ name=$(echo "$filename" | cut -f 1 -d '.')
+ ./a.c -i $filename -o $name.bin
+done
