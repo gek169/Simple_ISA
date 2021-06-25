9a39e5df10a318187180ec63578ec152485eb4b7
gek <6974902+gordonfreeman424@users.noreply.github.com>
2021-06-25 15:43:01 -0400

    Automatic commit.

---
+++ controlflow_1.asm
@@ -0,0 +1,17 @@
+#!/usr/local/bin/sisa16_asm -run
+//Example program from the manual to demonstrate basic control flow.
+
+getchar; 
+farstla %&0x20000%; 
+putchar;
+farllda %&0x20000%;
+lb 0x38; cmp; lb 0; cmp; sc %Lbl_val_is_lt%; jmpifeq;
+sc %Lbl_val_is_gte%; jmp;
+
+VAR#Lbl_val_is_lt#@
+	halt;
+VAR#Lbl_val_is_gte#@
+	farllda %&0x20000%;
+	cpc; 
+	putchar; 
+	jmp;
