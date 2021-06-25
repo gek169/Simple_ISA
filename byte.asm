4a76d3c98a1197cb0542536f0b657da5bc341f32
gek <6974902+gordonfreeman424@users.noreply.github.com>
2021-06-25 16:30:30 -0400

    Automatic commit.

---
+++ byte.asm
@@ -0,0 +1,52 @@
+#!/usr/local/bin/sisa16_asm -run
+//leave 10 bytes of free space at the beginning.
+section 0;
+	sc %10%; jmp;
+
+section 10;
+	getchar;
+//push it
+	apush;
+	lb 4;rsh;
+	lb0xf;and;
+	lb 7;mul;
+	llb %printbytehex_jmptable_1%; add;ca;jmp;
+	VAR#printbytehex_jmptable_1#@
+		la0x30;putchar;sc%printbytehex_jmptable_1_end%;jmp
+		la0x31;putchar;sc%printbytehex_jmptable_1_end%;jmp
+		la0x32;putchar;sc%printbytehex_jmptable_1_end%;jmp
+		la0x33;putchar;sc%printbytehex_jmptable_1_end%;jmp
+		la0x34;putchar;sc%printbytehex_jmptable_1_end%;jmp
+		la0x35;putchar;sc%printbytehex_jmptable_1_end%;jmp
+		la0x36;putchar;sc%printbytehex_jmptable_1_end%;jmp
+		la0x37;putchar;sc%printbytehex_jmptable_1_end%;jmp
+		la0x38;putchar;sc%printbytehex_jmptable_1_end%;jmp
+		la0x39;putchar;sc%printbytehex_jmptable_1_end%;jmp
+		la0x41;putchar;sc%printbytehex_jmptable_1_end%;jmp
+		la0x42;putchar;sc%printbytehex_jmptable_1_end%;jmp
+		la0x43;putchar;sc%printbytehex_jmptable_1_end%;jmp
+		la0x44;putchar;sc%printbytehex_jmptable_1_end%;jmp
+		la0x45;putchar;sc%printbytehex_jmptable_1_end%;jmp
+		la0x46;putchar;sc%printbytehex_jmptable_1_end%;jmp
+	VAR#printbytehex_jmptable_1_end#@
+	apop;lb 15;and;lb7;mul;
+	llb%printbytehex_jmptable_2%;add;ca;jmp;
+	VAR#printbytehex_jmptable_2#@
+		la0x30;putchar;sc%printbytehex_jmptable_2_end%;jmp
+		la0x31;putchar;sc%printbytehex_jmptable_2_end%;jmp
+		la0x32;putchar;sc%printbytehex_jmptable_2_end%;jmp
+		la0x33;putchar;sc%printbytehex_jmptable_2_end%;jmp
+		la0x34;putchar;sc%printbytehex_jmptable_2_end%;jmp
+		la0x35;putchar;sc%printbytehex_jmptable_2_end%;jmp
+		la0x36;putchar;sc%printbytehex_jmptable_2_end%;jmp
+		la0x37;putchar;sc%printbytehex_jmptable_2_end%;jmp
+		la0x38;putchar;sc%printbytehex_jmptable_2_end%;jmp
+		la0x39;putchar;sc%printbytehex_jmptable_2_end%;jmp
+		la0x41;putchar;sc%printbytehex_jmptable_2_end%;jmp
+		la0x42;putchar;sc%printbytehex_jmptable_2_end%;jmp
+		la0x43;putchar;sc%printbytehex_jmptable_2_end%;jmp
+		la0x44;putchar;sc%printbytehex_jmptable_2_end%;jmp
+		la0x45;putchar;sc%printbytehex_jmptable_2_end%;jmp
+		la0x46;putchar;sc%printbytehex_jmptable_2_end%;jmp
+	VAR#printbytehex_jmptable_2_end#@
+	halt;
