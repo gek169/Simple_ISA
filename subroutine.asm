749c6828d27fc40129fa0d60fbd6e90f3d5cfbf7
gek <6974902+gordonfreeman424@users.noreply.github.com>
2021-06-26 01:48:04 -0400

    Automatic commit.

---
+++ subroutine.asm
@@ -0,0 +1,72 @@
+#!/usr/local/bin/sisa16_asm -run
+
+section 0;
+	sc %L_main%; jmp;
+
+section 1000;
+VAR#proc_printbytehex#sc %1000%;call;
+	//retrieve our argument.
+	astp;lb3;sub;ca;ilda;
+	//push it
+	apush;
+	lb 4;rsh;
+	lb0xf;and;
+	lb 7;mul;
+	llb %printbytehex_jmptable_1%; add;ca;jmp;
+	VAR#printbytehex_jmptable_1#@
+		la0x30;putchar;sc%printbytehex_jmptable_1_end%;jmp
+		la0x31;putchar;sc%printbytehex_jmptable_1_end%;jmp
+		la0x32;putchar;sc%printbytehex_jmptable_1_end%;jmp
+		la0x33;putchar;sc%printbytehex_jmptable_1_end%;jmp
+		la0x34;putchar;sc%printbytehex_jmptable_1_end%;jmp
+		la0x35;putchar;sc%printbytehex_jmptable_1_end%;jmp
+		la0x36;putchar;sc%printbytehex_jmptable_1_end%;jmp
+		la0x37;putchar;sc%printbytehex_jmptable_1_end%;jmp
+		la0x38;putchar;sc%printbytehex_jmptable_1_end%;jmp
+		la0x39;putchar;sc%printbytehex_jmptable_1_end%;jmp
+		la0x41;putchar;sc%printbytehex_jmptable_1_end%;jmp
+		la0x42;putchar;sc%printbytehex_jmptable_1_end%;jmp
+		la0x43;putchar;sc%printbytehex_jmptable_1_end%;jmp
+		la0x44;putchar;sc%printbytehex_jmptable_1_end%;jmp
+		la0x45;putchar;sc%printbytehex_jmptable_1_end%;jmp
+		la0x46;putchar;sc%printbytehex_jmptable_1_end%;jmp
+	VAR#printbytehex_jmptable_1_end#@
+	apop;lb 15;and;lb7;mul;
+	llb%printbytehex_jmptable_2%;add;ca;jmp;
+	VAR#printbytehex_jmptable_2#@
+		la0x30;putchar;sc%printbytehex_jmptable_2_end%;jmp
+		la0x31;putchar;sc%printbytehex_jmptable_2_end%;jmp
+		la0x32;putchar;sc%printbytehex_jmptable_2_end%;jmp
+		la0x33;putchar;sc%printbytehex_jmptable_2_end%;jmp
+		la0x34;putchar;sc%printbytehex_jmptable_2_end%;jmp
+		la0x35;putchar;sc%printbytehex_jmptable_2_end%;jmp
+		la0x36;putchar;sc%printbytehex_jmptable_2_end%;jmp
+		la0x37;putchar;sc%printbytehex_jmptable_2_end%;jmp
+		la0x38;putchar;sc%printbytehex_jmptable_2_end%;jmp
+		la0x39;putchar;sc%printbytehex_jmptable_2_end%;jmp
+		la0x41;putchar;sc%printbytehex_jmptable_2_end%;jmp
+		la0x42;putchar;sc%printbytehex_jmptable_2_end%;jmp
+		la0x43;putchar;sc%printbytehex_jmptable_2_end%;jmp
+		la0x44;putchar;sc%printbytehex_jmptable_2_end%;jmp
+		la0x45;putchar;sc%printbytehex_jmptable_2_end%;jmp
+		la0x46;putchar;sc%printbytehex_jmptable_2_end%;jmp
+	VAR#printbytehex_jmptable_2_end#@
+	ret;
+
+
+section 500;
+	VAR#L_main#@
+		getchar;putchar;
+		lb 0x30;sub;
+		apush;
+		
+		la 0xa; putchar; la 0xd; putchar;
+		getchar;putchar;
+		lb 0x30;sub;
+		bpop;
+		mul;
+		apush;
+		la 0xa; putchar; la 0xd; putchar;
+		proc_printbytehex;
+		la 0xa; putchar; la 0xd; putchar;
+	halt;
