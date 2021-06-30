4dadef98c4eb11c30edf4efc1e17937b55e2a2da
gek <6974902+gordonfreeman424@users.noreply.github.com>
2021-06-30 13:52:08 -0400

    Developer time

--- assembler.c
+++ assembler.c
@@ -372,7 +372,6 @@ int main(int argc, char** argv){
 			puts("Optional argument: -fdis, --full-disassemble: disassemble a file, without ending on halts/illegal opcodes. Same semantics as -dis");
 			puts("Optional argument: -o: specify output file. If not specified it is: outsisa16.bin");
 			puts("Optional argument: -DBG: debug the assembler.");
-			puts("Optional argument: -m: print manual page to standard out. This is in case some jerk decided not to give you the manpage.");
 			puts("Optional argument: -E: Print macro expansion only do not write to file");
 			puts("Optional argument: -pl: Print lines");
 			puts("Optional argument: -C: display compiletime environment information (What C compiler you used) as well as Author.");
@@ -385,17 +384,6 @@ int main(int argc, char** argv){
 			printf("There are %d instructions in the instruction set for this version.\n", n_insns);
 			printf("The last instruction added to the instruction set is %s\n", insns[n_insns-1]);
 			return 0;
-		}
-		if(
-			strprefix("-m",argv[i]) ||
-			strprefix("--manual",argv[i]) ||
-			strprefix("--docs",argv[i]) ||
-			strprefix("--documentation",argv[i])
-		){
-			puts (
-#include "manpage.h"
-			);
-			exit(0);
 		}
 	}}
 	if(debugging) infile=stdin;
