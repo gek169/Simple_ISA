b61ceabf135ef993360b70ee66bf89e884ee2045
gek <6974902+gordonfreeman424@users.noreply.github.com>
2021-07-04 03:24:18 -0400

    Automatic commit.

---
+++ hello_friend.asm
@@ -0,0 +1,14 @@
+#!/usr/local/bin/sisa16_asm -run
+
+..include"libc.hasm"
+
+..(1):
+	la 12; apush; la 0; alpush;
+	proc_gets;
+	la 0xa; putchar; la 0xd; putchar;
+	proc_puts;
+	la 0xa; putchar; la 0xd; putchar;
+	halt;
+
+..(0):
+	la 1; lfarpc;
