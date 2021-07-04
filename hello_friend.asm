74f0fa3b7256266893078a49ea37cfffbce7c7c8
gek <6974902+gordonfreeman424@users.noreply.github.com>
2021-07-04 14:02:58 -0400

    Automatic commit.

--- hello_friend.asm
+++ hello_friend.asm
@@ -3,9 +3,12 @@
 ..include"libc.hasm"
 
 ..(1):
-	la 12; apush; la 0; alpush;
-//	proc_gets_noecho
+	la 0xc; apush; la 0; alpush;
 	proc_gets
+	la 0xa; putchar; la 0xd; putchar;
+	proc_puts;
+	la 0xa; putchar; la 0xd; putchar;
+	proc_puts;
 	la 0xa; putchar; la 0xd; putchar;
 	proc_puts;
 	la 0xa; putchar; la 0xd; putchar;
