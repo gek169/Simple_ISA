a40243bf6d91a2411d34a52c02cd404344276e9c
gek <6974902+gordonfreeman424@users.noreply.github.com>
2021-07-05 09:22:43 -0400

    Automatic commit.

---
+++ filt.asm
@@ -0,0 +1,66 @@
+#!/usr/local/bin/sisa16_asm -run
+
+..include"libc.hasm"
+
+
+..(0):
+la 1; lfarpc;
+
+..(1):
+	:filt_looptop:
+	getchar;
+	sc %filt_printbytehex%
+	apush;lb 0; cmp; bpop;				jmpifeq;
+	ab;apush;lb 1; cmp; bpop;			jmpifeq;
+	ab;apush;lb 2; cmp; bpop;			jmpifeq;
+	ab;apush;lb 3; cmp; bpop;			jmpifeq;
+	ab;apush;lb 4; cmp; bpop;			jmpifeq;
+	ab;apush;lb 5; cmp; bpop;			jmpifeq;
+	ab;apush;lb 6; cmp; bpop;			jmpifeq;
+	ab;apush;lb 7; cmp; bpop;			jmpifeq;
+	ab;apush;lb 8; cmp; bpop;			jmpifeq;
+	ab;apush;lb 11; cmp; bpop;			jmpifeq;
+	ab;apush;lb 12; cmp; bpop;			jmpifeq;
+	ab;apush;lb 14; cmp; bpop;			jmpifeq;
+	ab;apush;lb 15; cmp; bpop;			jmpifeq;
+	ab;apush;lb 16; cmp; bpop;			jmpifeq;
+	ab;apush;lb 17; cmp; bpop;			jmpifeq;
+	ab;apush;lb 18; cmp; bpop;			jmpifeq;
+	ab;apush;lb 19; cmp; bpop;			jmpifeq;
+	ab;apush;lb 20; cmp; bpop;			jmpifeq;
+	ab;apush;lb 21; cmp; bpop;			jmpifeq;
+	ab;apush;lb 22; cmp; bpop;			jmpifeq;
+	ab;apush;lb 22; cmp; bpop;			jmpifeq;
+	ab;apush;lb 23; cmp; bpop;			jmpifeq;
+	ab;apush;lb 24; cmp; bpop;			jmpifeq;
+	ab;apush;lb 25; cmp; bpop;			jmpifeq;
+	ab;apush;lb 26; cmp; bpop;			jmpifeq;
+	ab;apush;lb 27; cmp; bpop;			jmpifeq;
+	ab;apush;lb 28; cmp; bpop;			jmpifeq;
+	ab;apush;lb 29; cmp; bpop;			jmpifeq;
+	ab;apush;lb 30; cmp; bpop;			jmpifeq;
+	ab;apush;lb 31; cmp; bpop;			jmpifeq;
+//	ab;apush;lb 82; cmp; bpop;			jmpifeq;
+//	ab;apush;lb 81; cmp; bpop;			jmpifeq;
+	ab;apush;lb 10; cmp; bpop;			sc %filt_newline%;		jmpifeq;
+	ab;apush;lb 13; cmp; bpop;			jmpifeq;
+	ab;apush;lb 0xff; cmp; bpop;		sc %filt_end%;			jmpifeq;
+	ab;apush;lb 126;cmp;lb2;cmp;bpop;	sc %filt_printbytehex%;	jmpifeq;
+	ab;putchar;
+	sc %filt_looptop%; jmp;
+	:filt_newline:
+	la 0xa; putchar;
+	la 0xd; putchar;
+	sc %filt_looptop%; jmp;
+	:filt_printbytehex:
+	bpush
+	la 0xd; putchar
+	la 0xa; putchar
+	proc_printbytehex
+	bpop
+	sc %filt_looptop%; jmp;
+	:filt_end:
+	bpush
+	proc_printbytehex
+	bpop
+	halt
