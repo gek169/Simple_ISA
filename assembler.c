a38bd6c2979e57210668341172b3f32f34eaaff5
gek <6974902+gordonfreeman424@users.noreply.github.com>
2021-07-06 15:51:10 -0400

    Developer time

--- assembler.c
+++ assembler.c
@@ -1707,7 +1707,7 @@ int main(int argc, char** argv){
 						for(i = nbuiltin_macros; i < nmacros; i++){
 							if(!variable_is_redefining_flag[i])
 								if(int_checker(variable_expansions[i]) == 0)
-									fprintf(f, "VAR#%s#%s\r\n", variable_names[i], variable_expansions[i]);
+									fprintf(f, "VAR#%s#%s\n", variable_names[i], variable_expansions[i]);
 						}
 					}else{
 						printf(general_fail_pref);
