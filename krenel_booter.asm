6139ec907bb16778b424ac846f9b53cb21d2f93e
gek <6974902+gordonfreeman424@users.noreply.github.com>
2021-07-23 13:07:02 -0400

    Automatic commit.

--- krenel_booter.asm
+++ krenel_booter.asm
@@ -24,18 +24,33 @@ bytes '\r' ,'\n', 0;
 bytes '\r' ,'\n', 0;
 
 
-..main(22):
+..main:
 	lrx0 %/krenel_boot%;
 	proc_krenel;
 	lrx0 0, %&STR_my_other_string%;
 	rx0push;
 	proc_puts;
 	halt;
+..(8):
 	krenel_boot:
+		//Overwrite the krenel.
+		lrx0 %/0x20000%;
+		lrx1 %/0x20430%;
+		overwrite_krenel_looptop:
+			cbrx0;
+			la 1;
+			farista;
+			rxincr;
+			rxcmp;
+			sc %overwrite_krenel_looptop%; jmpifneq;
 		lrx0 0, %&STR_my_string%;
 		rx0push;
 		proc_puts;
 		la '\n'; syscall;
+		getchar;
+		getchar;
+		getchar;
+		getchar;
 		proc_gets_user;
 		la '\r'; putchar;
 		la '\n'; putchar;
