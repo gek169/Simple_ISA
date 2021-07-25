94f6d015a2b1ca5be9dfba5174ca314db1a2329f
gek <6974902+gordonfreeman424@users.noreply.github.com>
2021-07-24 20:37:25 -0400

    Automatic commit.

--- libc.asm
+++ libc.asm
@@ -1,7 +1,6 @@
-
-
+#!/usr/bin/sisa16_asm -run
+//The libc *is* actually a valid, bootable program.
+//see "libc_booter" in emulation.hasm.
 .LIBC_EXPORT_DCL:region 0;
-
 ..include"libc.hasm"
-
 ASM_EXPORT_HEADER
