3a522e558c9f95228fe54f63711227178b2b9346
gek <6974902+gordonfreeman424@users.noreply.github.com>
2021-07-31 18:41:42 -0400

    Developer time

--- isa_pre.h
+++ isa_pre.h
@@ -11,7 +11,11 @@ typedef long SUU;
 #endif
 static u R=0;
 
-#define SEGMENT_PAGES 0x40000
+/*
+	MUST BE POWER OF TWO!!!!
+	that means the lead digit must be 1, 2, 4, or 8, followed entirely by zeroes.
+*/
+#define SEGMENT_PAGES 0x400000
 
 /*This is important later!!! Only two privilege levels.
 0:		Kernel mode
