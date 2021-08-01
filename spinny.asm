30a646ec8ebc3b30c33ddc7c00e2c2733a53ca4a
gek <6974902+gordonfreeman424@users.noreply.github.com>
2021-08-01 15:29:53 -0400

    Automatic commit.

--- spinny.asm
+++ spinny.asm
@@ -2,34 +2,16 @@
 
 .ZERO_STACK_POINTER:		astp;popa;
 
-
-section 0x40000;
-
-
-.ITER_REGION:3
-..(ITER_REGION):
-	bytes 0,0,0,0;
-.ld_iter: farllda %&@&%
-.ld_iterb: farlldb %&@&%
-.st_iter: farstla %&@&%
-
 //..include"libc.hasm"
+
 ..include"libc_pre.hasm"
-..(2):
+..zero:
 ..dinclude"libc_pre.bin"
-.line_length:			90
-.line_length_plus_1:	91
-.num_lines:				20
 .wait_time:				50
-..main(3):
-	lrx0 0, %~LIBC_REGION%, %libc_COMMAND_COM%;
-	proc_krenel;
-	halt;
+
 ..(4):
 asm_begin_region_restriction;
 la '\r'; putchar; 
-la 0;
-st_iter;
 alpush;
 	//our loop!
 	.ascii_spinny_len:4
