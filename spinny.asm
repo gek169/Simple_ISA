d5a8c2658fd54dd8fb46f12000f77a67f839b6bd
gek <6974902+gordonfreeman424@users.noreply.github.com>
2021-08-04 01:20:28 -0400

    Automatic commit.

--- spinny.asm
+++ spinny.asm
@@ -5,24 +5,32 @@
 //..include"libc.hasm"
 
 ..include"libc_pre.hasm"
-..zero:
+..(2):
 ..dinclude"libc_pre.bin"
 .wait_time:				50
 
+..main(3):
+
+lrx0 %/krenel_boot%;
+proc_krenel;
+halt;
+
 ..(4):
 asm_begin_region_restriction;
+krenel_boot:
 la '\r'; putchar; 
 alpush;
 	//our loop!
-	.ascii_spinny_len:4
+	
 	asciifun_looptop: 
 		la ' '; putchar;
 		alpop; 
 			aincr; 
 		alpush;
 		
-		lb ascii_spinny_len;mod; 
-		sc %1%; lb %~ascii_spinny%; 
+		lb %~ascii_spinny_len%;mod; 
+		cpcr;
+		 lb %~ascii_spinny%; 
 		add; ba;
 		farilda;
 		putchar;
@@ -33,5 +41,5 @@ alpush;
 		sc %asciifun_looptop%;jmp;
 	:ascii_spinny:
 	..asciz:-\|/
-	
+	.ascii_spinny_len:4
 asm_end_restriction;
