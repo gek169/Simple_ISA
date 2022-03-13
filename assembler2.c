38568de7b41c86524e46223a7e9c21006b839f52
gek <6974902+gordonfreeman424@users.noreply.github.com>
2022-03-13 14:00:40 -0400

    gitap from gek@Katherine.

--- assembler2.c
+++ assembler2.c
@@ -3,183 +3,156 @@
 #include <string.h>
 #include <ctype.h>
 #include "isa_pre.h"
-unsigned char STRBUF[0x10000]; /*string buffer. Would be located in main memory.*/
-UU CUR_TOKEN_LEN = 0; /*Length of the current token.*/
-UU N_TOKENS = 0; /*Number of tokens received.*/
-UU TOKEN_IDENT = 0; /*current token identification. 0 means we need to determine what this is part of.*/
-/*
-	1- text (alphanum, period, and underscore)
-	2- number, optionally signed, optionally containing 0x.
-	3- unusual character
-*/
-const UU TOKEN_TEXT = 1;
-const UU TOKEN_NUM = 2;
-const UU TOKEN_UNU = 3; /*Preserved in case we decide to ever have any multi-character things.*/
-const UU TOKEN_FORWARDSLASH = 4;
-const UU TOKEN_STRING = 5; /*A string can be at most 64k characters. Is that a huge limitation?*/
-static UU ALLOCATED_PAGES = 0;
-u* SEGMENT = SEGS[0];
+unsigned char line[0x10000] = {0}; /*string buffer. Would be located in main memory.*/
 /*
 	standin for SISA16's SEG_REALLOC.
 */
 
 char istext(unsigned char c){
-	return isalpha(c) || (c == '_');
+	return isalpha(c) || (c == '_')
+		;
+}
+char isvarvalid(unsigned char c){
+	return isalpha(c) || (c == '_') || isdigit(c);
 }
 
-void pushToken(){
-	UU needed_space = (CUR_TOKEN_LEN + 255)>>8;
-	if(needed_space == 0) {
-		printf("<ERROR> empty token?\r\n");
-		exit(1);
+static unsigned char* read_until_terminator_alloced_modified(FILE* f, unsigned long* lenout){
+	char c;
+	unsigned char* const buf = line;
+	unsigned long bcap = 0x10000;
+	unsigned long blen = 0;
+
+	local_top_1:;
+	{
+		
+		if(feof(f)){goto local_end_1;}
+		c = fgetc(f);
+		if(c == '\n') {goto local_end_1;}
+		if(c == '\r') {goto local_end_1;}
+		if(blen == (bcap-1))	/*Grow the buffer.*/
+			{
+				exit(1);
+			}
+		buf[blen++] = c;
+		goto local_top_1;
+
+		local_end_1:;
 	}
-	if(ALLOCATED_PAGES + needed_space >= SEGMENT_PAGES){
-		printf("<Memory allocation error>");
-		exit(1);
-	}
-	memcpy(
-		SEGMENT+((ALLOCATED_PAGES)<<8),
-		STRBUF, 
-		needed_space * 256
-	);
-	ALLOCATED_PAGES += needed_space;
-	N_TOKENS++;
-	CUR_TOKEN_LEN = 0;
-	printf("<token %u, identification %u> %s\r\n", (unsigned int)N_TOKENS, (unsigned int)TOKEN_IDENT, SEGMENT+(SEGMENT_PAGES-needed_space)*256);
-	if(TOKEN_IDENT == TOKEN_TEXT || TOKEN_IDENT == TOKEN_STRING){
-		printf("%s\r\n", STRBUF);
-	}
-	TOKEN_IDENT = 0;
+	
+	buf[blen] = '\0';
+	*lenout = blen;
+	return buf;
 }
+
+typedef struct{
+	u type; 
+	/*
+		0 = byte		Promotion is Downward, exclusively
+		1 = u16
+		2 = s16
+		3 = u32
+		4 = s32
+		5 = f32
+	*/
+	u region;
+	U laddr;
+	U len; /*For arrays. 1 = normal variable*/
+	char name[128];
+}vardef;
+vardef variables[1000000];
+UU nvars = 0;
+UU codeaddr=0; /*Where does code go?*/
+UU varaddr =0x100000; /*Where do variables go?*/
+vardef q;
+UU i;
+UU i2;
+UU findvar(char* name){
+	for(i2 = 0; i2 < nvars; i2++) if(strcmp(name, variables[i2].name) == 0) return i2;
+	return nvars + 1;
+}
+UU is_keyword(char* name){
+	if(strcmp(name, "setcodeaddr") == 0) return 1;
+	if(strcmp(name, "setvaraddr") == 0) return 1;
+	if(strcmp(name, "def") == 0) return 1;
+	if(strcmp(name, "fun") == 0) return 1;
+	if(strcmp(name, "if") == 0) return 1;
+	if(strcmp(name, "while") == 0) return 1;
+	if(strcmp(name, "goto") == 0) return 1;
+	if(strcmp(name, "label") == 0) return 1;
+	if(strcmp(name, "u8") == 0) return 1;
+	if(strcmp(name, "i16") == 0) return 1;
+	if(strcmp(name, "u16") == 0) return 1;
+	if(strcmp(name, "i32") == 0) return 1;
+	if(strcmp(name, "u32") == 0) return 1;
+	if(strcmp(name, "f32") == 0) return 1;
+	if(strcmp(name, "rega") == 0) return 1;
+	if(strcmp(name, "regb") == 0) return 1;
+	if(strcmp(name, "regc") == 0) return 1;
+	if(strcmp(name, "reg0") == 0) return 1;
+	if(strcmp(name, "reg1") == 0) return 1;
+	if(strcmp(name, "reg2") == 0) return 1;
+	if(strcmp(name, "reg3") == 0) return 1;
+	if(strcmp(name, "load") == 0) return 1;
+	if(strcmp(name, "store") == 0) return 1;
+	if(strcmp(name, "push") == 0) return 1;
+	if(strcmp(name, "pop") == 0) return 1;
+	return 0;
+}
+UU pushVar(u t, UU len, char* name){
+	if(len == 0) return 0;
+	if(t > 5) return 0;
+	if(t == 0) {if(len > 65536) return 0;}
+	else if(t == 1 || t==2) {if(len >= 32768) return 0;}
+	else if(t == 3 || t == 4 || t == 5) {if(len >= 16384) return 0;}
+	if(strlen(name) > 127) return 0;
+	if(strlen(name) < 1) return 0;
+	if(!istext(*name)) return 0;
+	q.type = t;
+	q.region = varaddr / 0x10000;
+	q.laddr = varaddr & 0xffFF;
+	q.len = len;
+	
+	strcpy(q.name, name);
+	for(i = 0; i < 128; i++) if(!isvarvalid(q.name[i])) q.name[i] = 0;
+	if(strlen(q.name) == 0) return 0;
+	if(findvar(q.name) != (nvars + 1)) return 0;
+
+	/*Push variable allocation ahead*/
+	if(t == 0)
+		varaddr += len;
+	else if(t == 1 || t == 2)
+		varaddr += len * 2;
+	else if(t == 3 || t == 4 || t == 5)
+		varaddr += len * 4;
+	variables[nvars++]=q;return 1;
+}
+
+
+
 
 
 int main(){
+	unsigned char begin_char;
+	unsigned long lenout;
+	unsigned char* buf;
+	
 	/*
-		The program is received via stdin and is tokenized on-the-fly.
+		The program is received via stdin and is parsed as we go along.
 	*/
-	while(1){
-		unsigned char c = fgetc(stdin);
-		if(CUR_TOKEN_LEN > 0xffFF){
-			printf("<ERROR> Token exceeds size limit.");
-			exit(1);
-		}
-		if(feof(stdin)) break;
-		if(c == 0xFF) break;
-		if(c == 0) break;
-		/*fputc(c, stdout);*/
-		/*
-			IDENTIFIER CONTINUATIONS.
-		*/
-		if(c == '/' && CUR_TOKEN_LEN == 1 && TOKEN_IDENT == TOKEN_FORWARDSLASH){ /**/
-			while(!feof(stdin) && c != '\n') c = fgetc(stdin);
-			if(feof(stdin)) break;
-			if(c == 0xFF) break;
-			if(c == 0) break;
-			CUR_TOKEN_LEN = 0;
-			continue;
-		} else if(TOKEN_IDENT == TOKEN_FORWARDSLASH){
-			STRBUF[CUR_TOKEN_LEN++] = '\0';
-			pushToken();
-		} else if(TOKEN_IDENT == TOKEN_TEXT){
-			if(!istext(c) && !isdigit(c)){
-				STRBUF[CUR_TOKEN_LEN++] = '\0';
-				pushToken();
-			} else {
-				STRBUF[CUR_TOKEN_LEN++] = c;
-				continue;
-			}
-		} else if(TOKEN_IDENT == TOKEN_NUM){
-			if(c == '0'
-				|| c == '1'
-				|| c == '2'
-				|| c == '3'
-				|| c == '4'
-				|| c == '5'
-				|| c == '6'
-				|| c == '7'
-				|| c == '8'
-				|| c == '9'
-				|| c == 'a' || c == 'A'
-				|| c == 'b' || c == 'B'
-				|| c == 'c' || c == 'C'
-				|| c == 'd' || c == 'D'
-				|| c == 'e' || c == 'E'
-				|| c == 'f' || c == 'F'
-				|| c == 'x' /*Hex 0x*/
-				|| c == '.' /*Float decimal point*/
-			){
-				STRBUF[CUR_TOKEN_LEN++] = c;
-				continue;
-			} else {
-				STRBUF[CUR_TOKEN_LEN++] = '\0';
-				pushToken();
-			}
-		}
-		/*
-			IDENTIFIER STARTERS.
-		*/
-		if(c == '\r' ||
-		   c == ' '  ||
-		   c == '\t' ||
-		   c == '\f' ||
-		   c == '\n' ||
-		   c == '\e' ||
-		   c == '\v' ||
-		   c < 32) 
-		{
-			if(CUR_TOKEN_LEN) pushToken();
-			TOKEN_IDENT = 0;
-			CUR_TOKEN_LEN = 0;
-			continue;
-		} 
-		else if(isdigit(c) && CUR_TOKEN_LEN == 0){
-			STRBUF[CUR_TOKEN_LEN++] = c;
-			TOKEN_IDENT = TOKEN_NUM;
-			continue;
-		}
-		else if(istext(c) && CUR_TOKEN_LEN == 0){
-			STRBUF[CUR_TOKEN_LEN++] = c;
-			TOKEN_IDENT = TOKEN_TEXT;
-			continue;			
-		}
-		else if(c == '"' && CUR_TOKEN_LEN == 0){
-			/*STRBUF[CUR_TOKEN_LEN++] = c; Don't even store the damned thing.*/
-			TOKEN_IDENT = TOKEN_STRING;
-			while(!feof(stdin)) {
-				c = fgetc(stdin);
-				if(c == '"') break;
-				if(c == 0xFF) break;
-				if(c == 0) break;
-				STRBUF[CUR_TOKEN_LEN++] = c;
-				if(CUR_TOKEN_LEN > 0xffFF){
-					printf("<ERROR> String token exceeds size limit.");
-					exit(1);
-				}
-			}
-			if(feof(stdin)
-			||(c == 0xFF) 
-			||(c == 0)){
-				printf("<ERROR> Unterminated string literal.");
-				exit(1);
-			} 
-			continue;
-		}else if(c == '/' && CUR_TOKEN_LEN == 0){
-			STRBUF[CUR_TOKEN_LEN++] = c;
-			TOKEN_IDENT = TOKEN_FORWARDSLASH;
-			continue;			
-		}else if(CUR_TOKEN_LEN == 0){
-			STRBUF[0] = c;
-			STRBUF[1] = '\0';
-			CUR_TOKEN_LEN = 2;
-			TOKEN_IDENT = TOKEN_UNU;
-			pushToken();
-			continue;
-		}
+	begin_line:;
+	read_until_terminator_alloced_modified(stdin, &lenout);
+	buf = line;
+	while(*buf == ' ' || *buf == '\t' || *buf == '\v') buf++; /*Skip preceding whitespace*/
+	begin_char = line[0];
+	if(begin_char == 0xff) return 0;
+	if(begin_char == '$') goto dump_line;
+	if(begin_char == 0) goto begin_line;
+	/*BASIC PROCESSING!!!*/
+	return 0; /*TODO*/
 
-		printf("<TOKENIZER ERROR> unhandled case.\r\n");
-		exit(1);
-
-	}
-
+	goto begin_line;
+	/*It was an assembly line!*/
+	dump_line:puts((char*)line+1);goto begin_line;
+	
 	return 0;
 }
